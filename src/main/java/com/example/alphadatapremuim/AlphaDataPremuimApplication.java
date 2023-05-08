f6dfb8af7397d9f23910a1c24501cce2d96fcd9c
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 16:28:13 +0000

    update...

---
+++ src/main/java/com/example/alphadatapremuim/AlphaDataPremuimApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphadatapremuim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaDataPremuimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaDataPremuimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphadatapremuim/AlphaDataPremuimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphadatapremuim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaDataPremuimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
